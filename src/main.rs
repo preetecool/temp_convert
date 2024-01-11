99d527a7faf235f6ee713ca58e67fdafe21144b5
preetecool <preet@creal.ca>
2024-01-10 19:42:57 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::io;
 
 fn main() {
-let unit = unit_type();
+let unit = get_unit_type();
 
 if unit == "Invalid" {
     return;
 }
 else {
-    println!("You have chosen to convert from {}", unit);
+    println!("You have chosen to convert from {unit}");
     convert_temp(unit);
 }
-// Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
-// Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9   
+
+
 }
 
-fn unit_type() -> &'static str  {
+fn get_unit_type() -> &'static str  {
     let mut count = 0;
     let mut unit = String::new();
     while count < 3 {
@@ -23,24 +23,35 @@ fn unit_type() -> &'static str  {
         .read_line(&mut unit)
         .expect("Failed to read line");
 
-        let unit = match unit.trim().to_lowercase().as_str() {
-            "c" => "Celsius",
-            "f" => "Fahrenheit",
-            _ => {
-                println!("Please enter a valid unit type");
+        let mut unit = unit.trim().to_lowercase();
+
+        match validate_unit_type(&unit) {
+            Ok(valid_unit) => return valid_unit,
+            Err(message) => {
+                println!("{}", message);
                 println!("You have {} attempts remaining", 2 - count);
                 count += 1;
-                continue;
-              
+                unit.clear();
             }
         };
-        return unit;
+     
     }
-    return "Invalid"; 
+   return "Invalid"; 
 }
+fn validate_unit_type(unit: &str) -> Result<&'static str, &'static str> {
+    match unit.to_lowercase().as_str() {
+        "c" => Ok("Celsius"),
+        "f" => Ok("Fahrenheit"),
+        _ => Err("Please enter a valid unit type"),
+    }
+}
+
+
 fn convert_temp(unit: &str){
     let mut count = 0;
+
     println!("Please enter the temperature you would like to convert");
+
     while count < 3 {
         let mut temp = String::new();
         
@@ -56,14 +67,23 @@ fn convert_temp(unit: &str){
                 continue;
             }
         };
-        if unit == "Celsius" {
-            let fahrenheit = (temp * 9.0/5.0) + 32.0;
-            return println!("{} degrees Celsius is {} degrees Fahrenheit", temp, fahrenheit);
-        } else if unit == "Fahrenheit" {
-            let celsius = (temp - 32.0) * 5.0/9.0;
-            return println!("{} degrees Fahrenheit is {} degrees Celsius", temp, celsius);
-        } else {
-            return println!("Invalid unit type");
+       match unit {
+        // Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9   
+            "Celsius" => {
+                let fahrenheit = (temp * 9.0 / 5.0) + 32.0;
+                println!("{} degrees Celsius is {} degrees Fahrenheit", temp, fahrenheit);
+                return;
+            }
+        // Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+            "Fahrenheit" => {
+                let celsius = (temp - 32.0) * 5.0 / 9.0;
+                println!("{} degrees Fahrenheit is {} degrees Celsius", temp, celsius);
+                return;
+            }
+            _ => {
+                println!("Invalid unit type");
+                return;
+            }
         }
     }
   
